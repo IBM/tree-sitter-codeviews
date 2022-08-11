3f8a2a640260a45f4282747894593d02e52712c3
Alex Mathai <Alex.Mathai1@ibm.com>
2022-08-11 10:38:16 +0530

    modifying setup
    Signed-off-by: Noble Mathews <55821103+NobleMathews@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -14,11 +14,11 @@ git clone https://github.com/tree-sitter/tree-sitter-python.git
 
 # Uncomment the following lines according to the source languages you want to work with using tree-sitter
 # Currently this repository supports only Java source code
-# git clone https://github.com/tree-sitter/tree-sitter-c-sharp.git
-# git clone https://github.com/tree-sitter/tree-sitter-go.git
-# git clone https://github.com/tree-sitter/tree-sitter-javascript.git
-# git clone https://github.com/tree-sitter/tree-sitter-php.git
-# git clone https://github.com/tree-sitter/tree-sitter-ruby.git
+git clone https://github.com/tree-sitter/tree-sitter-c-sharp.git
+git clone https://github.com/tree-sitter/tree-sitter-go.git
+git clone https://github.com/tree-sitter/tree-sitter-javascript.git
+git clone https://github.com/tree-sitter/tree-sitter-php.git
+git clone https://github.com/tree-sitter/tree-sitter-ruby.git
 
 cd ..
 
